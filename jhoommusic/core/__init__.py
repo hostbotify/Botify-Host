6bd8685a4b8e67d482c0044aac38f8f95817bb5f
hostbotify <hostbotify@gmail.com>
2025-07-09 18:44:34 +0530

    Update __init__.py

--- jhoommusic/core/__init__.py
+++ jhoommusic/core/__init__.py
@@ -1,13 +1,13 @@
 from .bot import app, pytgcalls
 from .config import Config
-from .database import Database
-from .connection import ConnectionManager
-from .queue import QueueManager
-from .process import ProcessManager
+from .database import db as Database
+from .connection import connection_manager as ConnectionManager
+from .queue import queue_manager as QueueManager
+from .process import process_manager as ProcessManager
 
 __all__ = [
     "app",
-    "pytgcalls", 
+    "pytgcalls",
     "Config",
     "Database",
     "ConnectionManager",
